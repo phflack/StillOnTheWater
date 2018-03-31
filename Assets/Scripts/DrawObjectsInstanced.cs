87a6b076c06a4df0dbdda57fb8362db72ea9c879
James Swanick <swanijam@gmail.com>
2018-03-31 10:09:58 -0400

    cubemap skybox wips

---
+++ Assets/Scripts/DrawObjectsInstanced.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawObjectsInstanced : MonoBehaviour {
+	public static List<Matrix4x4> transformationMatrices;
+
+	public Mesh mesh;
+	int count = 0;
+	public Material mat;
+
+	// Use this for initialization
+	void Start () {
+		count ++;
+		transformationMatrices.Add (Matrix4x4.TRS (transform.position, transform.rotation, transform.localScale));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Graphics.DrawMeshInstanced(mesh,0, mat, transformationMatrices, null, UnityEngine.Rendering.ShadowCastingMode.On);
+	}
+}
