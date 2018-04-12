b2036057da1fed0d1442fef7fb92188106d6fb5d
swanijam <swanijam@gmail.com>
2018-04-11 21:11:27 -0400

    Fixing trees that disappear after too many trees are instanced.

--- Assets/Scripts/DrawObjectsInstanced.cs
+++ Assets/Scripts/DrawObjectsInstanced.cs
@@ -20,28 +20,41 @@ public class DrawObjectsInstanced : MonoBehaviour {
 	public Material mat;
 
 	// when set to true, transform matrix is recorded in Start(). otherwise, it is caused by RecordTransformation();
-	public bool RecordTransformationOnStart = false; 
+	public bool RecordTransformationOnStart = false;
 
 
-	// Use this for initialization
-	void Start () {
-		if (meshes == null || transformationMatriceses == null || materials == null) {
-			Debug.Log ("Initializing GPU Instance matrices...");
-			meshes = new Dictionary<string, Mesh> ();
-			transformationMatriceses = new Dictionary<string, List<Matrix4x4>> ();
-			materials = new Dictionary<string, Material> ();
-		}
-		if (!meshes.ContainsKey (identifier)) {
-			Debug.Log ("Adding GPU Instance group key: " + identifier + " - from object: " + gameObject.name);
-			meshes [identifier] = mesh;
-			transformationMatriceses [identifier] = new List<Matrix4x4> ();
-			materials [identifier] = mat;
-		}
+    // Use this for initialization
+    void Start()
+    {
+        if (meshes == null || transformationMatriceses == null || materials == null)
+        {
+            Debug.Log("Initializing GPU Instance matrices...");
+            meshes = new Dictionary<string, Mesh>();
+            transformationMatriceses = new Dictionary<string, List<Matrix4x4>>();
+            materials = new Dictionary<string, Material>();
+        }
+        if (!meshes.ContainsKey(identifier))
+        {
+            Debug.Log("Adding GPU Instance group key: " + identifier + " - from object: " + gameObject.name);
+            meshes[identifier] = mesh;
+            transformationMatriceses[identifier] = new List<Matrix4x4>();
+            materials[identifier] = mat;
+        }
 
-		if (RecordTransformationOnStart || true) {
-			transformationMatriceses [identifier].Add (Matrix4x4.TRS (transform.position, transform.rotation, transform.lossyScale));
-		}
-	}
+        if (RecordTransformationOnStart || true)
+        {
+            //oversimplified pruning of the list of trees. Chops of 650 trees  each time.
+            if (transformationMatriceses[identifier].Count >= 1000)
+            {
+                for (int i = 0; i < transformationMatriceses[identifier].Count - 650; i++)
+                {
+                    transformationMatriceses[identifier][i] = transformationMatriceses[identifier][i + 650];
+                }
+                transformationMatriceses[identifier].RemoveRange(transformationMatriceses[identifier].Count - 650, 650);
+            }
+            transformationMatriceses[identifier].Add(Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale));
+        }
+    }
 
 	public void RecordTransformation() {
 		Debug.Log ("Recording Transformations");
