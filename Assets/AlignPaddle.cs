5efeab613dcc12f82326f0358f41c072a64b4c03
James Swanick <swanijam@gmail.com>
2018-05-17 18:15:47 -0400

    paaaaaaddddle

---
+++ Assets/AlignPaddle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignPaddle : MonoBehaviour {
+	public Transform leftHandle;
+	public Transform rightHandle;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 handlesSeparation = rightHandle.position - leftHandle.position;
+		transform.position = leftHandle.position += handlesSeparation / 2;
+		transform.LookAt (leftHandle.position);
+	}
+}
