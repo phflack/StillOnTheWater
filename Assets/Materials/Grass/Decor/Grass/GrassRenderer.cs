dc32de9bd2231bf4e95202c7f5a2236287effe15
James Swanick <swanijam@gmail.com>
2018-06-25 14:20:01 -0400

    Manually Carved Terrain

--- Assets/Materials/Grass/Decor/Grass/GrassRenderer.cs
+++ Assets/Materials/Grass/Decor/Grass/GrassRenderer.cs
@@ -16,6 +16,7 @@ public class GrassRenderer : MonoBehaviour {
     public float startHeight = 1000;
     public float GrassOffSet = 0f;
 	public LayerMask mask;
+	public float hitNormalThreshold;
 
 	Vector3 origin;
 	Ray ray;
@@ -25,7 +26,8 @@ public class GrassRenderer : MonoBehaviour {
     // Use this for initialization
     void Start () {
 		origin = transform.position;
-		StartCoroutine (GrassUpdate ());
+//		StartCoroutine (GrassUpdate ());
+		StartCoroutine(GrassUpdate());
 	}
 	
 	// Update is called once per frame
@@ -39,11 +41,13 @@ public class GrassRenderer : MonoBehaviour {
 			ray = new Ray(new Vector3(origin.x + size.x * Random.Range(-.5f,.5f), startHeight,origin.z + size.y * Random.Range(-.5f,.5f)), Vector3.down);
             if(Physics.Raycast(ray, out hit,1000f,mask))
             {
-				matrices.Add(Matrix4x4.TRS((hit.point + (Vector3.up * GrassOffSet)), Quaternion.identity, Vector3.one));
+				if (Vector3.Dot (hit.normal, Vector3.up) > hitNormalThreshold) {
+					matrices.Add (Matrix4x4.TRS ((hit.point + (Vector3.up * GrassOffSet)), Quaternion.identity, Vector3.one));
+				}
             }
         }
 		yield return new WaitForSeconds (2f);
-		StartCoroutine(GrassUpdate());
+//		StartCoroutine(GrassUpdate());
 	}
 
 	void Update() {
