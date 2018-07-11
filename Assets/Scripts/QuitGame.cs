4f6deb772b243462d49e22a7211959e8da8aa75e
swanijam <swanijam@gmail.com>
2018-07-11 16:22:27 -0400

    start menu timeline wip

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+    public void Quit()
+    {
+               // save any game data here
+       #if UNITY_EDITOR
+               // Application.Quit() does not work in the editor so
+               // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+               UnityEditor.EditorApplication.isPlaying = false;
+       #else
+                Application.Quit();
+       #endif
+    }
+}
