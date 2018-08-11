9b66178f1e1434039fd4e08d6bd3ef6621620cc4
PIXEL\Inky <tinysquiddle@gmail.com>
2018-08-10 23:34:19 -0400

    Added a lot more fish that rotate around

---
+++ Assets/gizbo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gizbo : MonoBehaviour {
+
+	// Use this for initializationvoid
+	/// <summary>
+	/// Callback to draw gizmos that are pickable and always drawn.
+	/// </summary>
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawSphere(transform.position, 1f);
+	}
+}
