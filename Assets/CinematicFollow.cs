7696b5696c9169a7ce79bdf6d6dd316aafb17c76
Erin's Desktop <nern.strudel@gmail.com>
2019-02-03 16:15:46 -0500

    whee

---
+++ Assets/CinematicFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicFollow : MonoBehaviour {
+
+    public bool isStarted = false;
+    public float speed;
+
+    public Cinemachine.CinemachineTrackedDolly vCam;
+
+	// Use this for initialization
+	void Start () {
+        vCam = GetComponent<Cinemachine.CinemachineTrackedDolly>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && !isStarted)
+        {
+            isStarted = true;
+        }
+
+        if(isStarted)
+        {
+            vCam.m_PathPosition += speed * Time.deltaTime;
+
+        }
+	}
+}
