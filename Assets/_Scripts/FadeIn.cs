cd8316f4a3dd20043e95663dee9f69c2982301b1
Erin's Laptop <nern.strudel@gmail.com>
2019-02-21 15:59:00 -0500

    hotkeys! supports using both alpha keyboard and keypad to travel to up to 9 hotkey locations with nice fading yay!

--- Assets/_Scripts/FadeIn.cs
+++ Assets/_Scripts/FadeIn.cs
@@ -7,16 +7,47 @@ public class FadeIn : MonoBehaviour {
     public float fadeTime = 0.8f;
     public Material m;
 
+    public bool fadingIn= false;
+    public bool fadingOut = false;
+
 	// Use this for initialization
 	void Start () {
         m.color = new Color(0, 0, 0, 1f);
+        fadingIn = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (m.color.a > 0)
+        if (m.color.a > 0 && fadingIn)
         {
             m.color = new Color(0, 0, 0, m.color.a - ((1 / fadeTime) * Time.deltaTime));
         }
+        else if(m.color.a <= 0 && fadingIn)
+        {
+            fadingIn = false; //we're done here
+        }
+        else if(fadingOut)
+        {
+            if (m.color.a < 1)
+            {
+                m.color = new Color(0, 0, 0, m.color.a + ((1 / fadeTime) * Time.deltaTime));
+            }
+            else //we're done
+            {
+                fadingOut = false;
+            }
+
+
+        }
+    }
+
+    public void fadeIn()
+    {
+        fadingIn = true;
+    }
+
+    public void fadeOut()
+    {
+        fadingOut = true;
     }
 }
