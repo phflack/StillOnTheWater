8b8f7a4096a1c9c9385c728f6578b8bb6d76b211
Erin's Laptop <nern.strudel@gmail.com>
2019-03-14 07:46:35 -0400

    at self, many things

---
+++ Assets/Artifact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Artifact : MonoBehaviour {
+    public MatchPositionAndRotationOverTime mover;
+    public ArtifactManager parent;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<VRTK_InteractableObject>().InteractableObjectUngrabbed += new InteractableObjectEventHandler(addThis);
+    }
+
+    public void addThis(object sender, InteractableObjectEventArgs e)
+    {
+        parent.addArtifact(this);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/ObjectFly.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ObjectFly : MonoBehaviour
+{
+    public bool playNow = false;
+    bool played = false;
+    public Transform triggerer;
+    public float radius = 5f;
+    public ArtifactManager artifacts;
+    public Transform[] positions;
+   
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            for(int i = 0; i< positions.Length; i++)
+            {
+                artifacts.artifacts[i].GetComponent<MatchPositionAndRotationOverTime>().target = positions[i];
+                artifacts.artifacts[i].GetComponent<MatchPositionAndRotationOverTime>().GoNow();
+            }
+            played = true;
+            this.enabled = false;
+        }
+        if (!played && triggerer != null && Vector3.Distance(transform.position, triggerer.position) < radius)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                artifacts.artifacts[i].GetComponent<MatchPositionAndRotationOverTime>().target = positions[i];
+                artifacts.artifacts[i].GetComponent<MatchPositionAndRotationOverTime>().GoNow();
+            }
+            played = true;
+        }
+    }
+
+}
