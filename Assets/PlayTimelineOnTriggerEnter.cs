f6d4375c41f5d80a2a28ae267c2ad15ae52ddc39
swanijam <swanijam@gmail.com>
2019-03-14 00:08:40 -0400

    Snake Scene 2

---
+++ Assets/PlayTimelineOnTriggerEnter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlayTimelineOnTriggerEnter : MonoBehaviour {
+	public PlayableDirector playableDirector;
+	public Transform triggerer;
+	public float radius=  5f;
+
+	private void Start()
+	{
+		if (playableDirector == null) playableDirector = GetComponent<PlayableDirector>();
+	}
+	public bool playNow = false;
+	bool played = false;
+	private void Update()
+	{
+		if (playNow) {
+			playNow = false;
+			playableDirector.Play();
+			played = true;
+			this.enabled = false;
+		}
+		if (!played && Vector3.Distance(transform.position, triggerer.position) < radius) {
+			playableDirector.Play();
+			played = true;
+			this.enabled = false;
+		}		
+	}
+
+	public bool drawGizmos = true;
+	private void OnDrawGizmos()
+	{
+		if (drawGizmos) {
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(transform.position, radius);
+		}
+	}
+}
