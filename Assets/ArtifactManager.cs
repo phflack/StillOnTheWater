5d65d72ca0e9ab35f2e4495a360e39e3f9d3e8a7
Erin's Desktop <nern.strudel@gmail.com>
2019-03-17 16:08:40 -0400

    item pickup! flying bois!!!!!

--- Assets/ArtifactManager.cs
+++ Assets/ArtifactManager.cs
@@ -17,6 +17,8 @@ public class ArtifactManager : MonoBehaviour {
     {
         artifact.mover.target = bones[index].transform;
         artifact.mover.GoNow();
+        artifact.transform.parent = bones[index].transform;
+        artifacts[index] = artifact.gameObject;
         index++;
     }
 }
