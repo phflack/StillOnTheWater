e26b989332a96cf73a06ad1edf33d84242e426b2
DESKTOP-EK807HG\tinys <tinysquiddle@gmail.com>
2019-03-18 00:24:37 -0400

    merge

--- Assets/PushOffDirt.cs
+++ Assets/PushOffDirt.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PushOffDirt : MonoBehaviour {
+	public AudioClip[] impactsounds;
+	public AudioSource source;
 	public Transform refTransform;
 	public Rigidbody target;
 	public Vector3 direction = Vector3.right;
@@ -13,10 +15,34 @@ public class PushOffDirt : MonoBehaviour {
 	{
 		lastPos = refTransform.position;
 	}
+	public ParticleSystem particleSystem;
+	public Vector2 numParticlesRange = new Vector2(2, 5);
 	// Update is called once per frame
+	bool _makingContact;
+	bool makingContact {
+		get {
+			return makingContact;
+		}
+		set {
+			if (value != _makingContact) {
+				if (value) {
+					if (particleSystem!= null) particleSystem.Emit((int)Random.Range(numParticlesRange.x, numParticlesRange.y));
+					if (source != null && impactsounds != null && impactsounds.Length > 0) source.PlayOneShot(impactsounds[Random.Range(0, impactsounds.Length-1)]);
+				} else {
+					if (particleSystem!= null) particleSystem.Emit((int)numParticlesRange.x);
+				}
+				_makingContact = value;
+			}
+		}
+	}
+
+	private void Start()
+	{
+		if (source == null) source = GetComponent<AudioSource>();
+	}
 	void LateUpdate () {
 		RaycastHit hit;
-		if (Physics.Raycast(transform.position, transform.TransformDirection(direction), out hit, raycastDistance, terrainLayers)) {
+		if (makingContact= Physics.Raycast(transform.position, transform.TransformDirection(direction), out hit, raycastDistance, terrainLayers)) {
 			Vector3 translation = -(refTransform.position - lastPos).XZVector(); // hand delta
 			translation *= Mathf.Clamp01(Vector3.Dot(translation.normalized, hit.normal));
 			//Debug.Log(Vector3.Dot(translation, hit.normal));
